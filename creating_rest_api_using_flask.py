030e05b53fd8588610889306115f26f1a0c613a6
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-20 23:00:06 +0800

    legit done installing the flask in my venv kahit check mo pa sa pip list

---
+++ creating_rest_api_using_flask.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify  
+
+app = Flask(__name__)
+
+my_info = {
+    "name": "Sheena Mae Delima",
+    "course_and_section": "BSCPE 1-2",
+    "favorite_programming language": "Python",
+    "aws_service": "Amazon S3 (Simple Storage Service)"
+}
+
+@app.route('/', methods=['GET'])
+def get_info():
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
